cbeab43940890e5189ccd3f7507e5639cb519625
Kevin <kevin.kipkoech@student.moringaschool.com>
2022-12-07 10:01:36 +0300

    style card holder

--- client/src/MyFavorites.js
+++ client/src/MyFavorites.js
@@ -20,15 +20,13 @@ export default function MyFavorites() {
         )
     })
 
-    
-   
     return (
         <div className="my-favs">
             <div className="my-favs-header">
                 <h1>My Favorites</h1>
                 <button className="btn">Create New Fav</button>
             </div>
-            <div className="cards-section">
+            <div className="cards--section">
                     {cardInfo}
             </div>
         </div>
